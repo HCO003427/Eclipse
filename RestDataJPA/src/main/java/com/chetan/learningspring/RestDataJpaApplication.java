b22e43f1e0d2a7e3b20534f46477a19b09eb6236
Chetan Kukreja <kukreja.chetan@yahoo.com>
2018-10-21 19:32:39 +0530

    Creating Base Project

---
+++ RestDataJPA/src/main/java/com/chetan/learningspring/RestDataJpaApplication.java
@@ -0,0 +1,12 @@
+package com.chetan.learningspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDataJpaApplication.class, args);
+	}
+}
